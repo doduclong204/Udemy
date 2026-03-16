95815dfee6a938d947b3b5a182f22efbba750c7d
doduclong204 <doduclong2004gg@gmail.com>
2026-03-16 13:25:31 +0700

    update

--- backend/src/test/java/com/education/udemy/UdemyApplicationTests.java
+++ backend/src/test/java/com/education/udemy/UdemyApplicationTests.java
@@ -2,8 +2,10 @@ package com.education.udemy;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("ci")
 class UdemyApplicationTests {
 
 	@Test
